35a08b683169c58c45a001de4e05c14cbee344cd
Ermira Bajrami <ermirabajrami91@gmail.com>
2023-06-19 13:46:17 +0200

    language saved

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -8,24 +8,22 @@ use Illuminate\Support\Facades\Auth;
 class AuthService
 {
     public function createAuth(AuthRequest $authRequest)
-{
-    $credentials = $authRequest->validated();
+    {
+        $credentials = $authRequest->validated();
 
-    if (Auth::attempt($credentials)) {
-        session()->regenerate();
+        if (Auth::attempt($credentials)) {
+            session()->regenerate();
 
-        $user = Auth::user();
+            $user = Auth::user();
 
-        // Retrieve the language ID using the UserLanguage model
-        $languageId = UserLanguage::where('user_id', $user->id)->value('language_id');
+            $languageId   = UserLanguage::where('user_id', $user->id)->value('language_id');
+            $responseData = [
+                'message'     => __('messages.welcome'),
+                'user'        => $user,
+                'language_id' => $languageId,
+            ];
 
-        $responseData = [
-            'message'     => __('messages.welcome'),
-            'user'        => $user,
-            'language_id' => $languageId,
-        ];
-
-        return $responseData;
+            return $responseData;
+        }
     }
-}
 }
