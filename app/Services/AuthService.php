31da1f643122c4b0a7db1865c288cdcefd85389a
Ermira Bajrami <ermirabajrami91@gmail.com>
2023-06-19 15:40:45 +0200

    notification login

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,6 +3,7 @@ namespace App\Services;
 
 use App\Http\Requests\AuthRequest;
 use App\Models\UserLanguage;
+use App\Models\UserNotification;
 use Illuminate\Support\Facades\Auth;
 
 class AuthService
@@ -16,11 +17,13 @@ class AuthService
 
             $user = Auth::user();
 
-            $languageId   = UserLanguage::where('user_id', $user->id)->value('language_id');
-            $responseData = [
-                'message'     => __('messages.welcome'),
-                'user'        => $user,
-                'language_id' => $languageId,
+            $languageId     = UserLanguage::where('user_id', $user->id)->value('language_id');
+            $notificationId = UserNotification::where('user_id', $user->id)->value('system');
+            $responseData   = [
+                'message'        => __('messages.welcome'),
+                'user'           => $user,
+                'language_id'    => $languageId,
+                'notificationId' => $notificationId,
             ];
 
             return $responseData;
