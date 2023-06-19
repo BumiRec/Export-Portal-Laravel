1bf9d1cab1af211b0046e03fda7eb0bd6d4e0a07
ElsaRamadani <125298497+ElsaRamadani@users.noreply.github.com>
2023-06-19 14:58:58 +0200

    Pagination

--- app/Services/ExportService.php
+++ app/Services/ExportService.php
@@ -24,7 +24,7 @@ class ExportService
                 'p.created_at'])
             ->paginate(10);
 
-        return ExportResource::collection($exportProducts);
+        return ($exportProducts);
 
     }
 
