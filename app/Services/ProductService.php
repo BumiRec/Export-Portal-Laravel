c57272f1b37a5bea4ab0384dd58cea4cce425ae8
ElsaRamadani <125298497+ElsaRamadani@users.noreply.github.com>
2023-06-19 15:40:23 +0200

    Pagination

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -6,11 +6,17 @@ class ProductService{
 
     public function productList(){
 
-        return Product::join('product_category as pc', 'product.category_id', 'pc.id')
+        $product = Product::join('product_category as pc', 'product.category_id', 'pc.id')
         ->join('product_subcategory as ps', 'product.subcategory_id', 'ps.id')
         ->join('company as c', 'product.company_id', 'c.id')
         ->select(['product.name', 'product.description', 'product.price', 'product.type',
         'pc.name as category', 'ps.name as subcategory', 'c.name as company'])
         ->paginate(10);
+
+        $numPages = $product->lastPage();
+        return [
+            'numPages' => $numPages,
+            'exportProducts' => $product,
+        ];
     }
 }
