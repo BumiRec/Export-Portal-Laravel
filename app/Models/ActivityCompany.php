2b2ba8e0b6aef7f87cde2358bb8c595a635d1be4
GERI12326 <114013384+GERI12326@users.noreply.github.com>
2023-06-22 13:28:19 +0200

    test

--- app/Models/ActivityCompany.php
+++ app/Models/ActivityCompany.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class ActivityCompany extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     public $table       = 'activity_company';
     protected $fillable = ['activity_area_id', 'company_id'];
