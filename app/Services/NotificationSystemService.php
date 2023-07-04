e882a326065d885d12ff26e17e865cdcae1553f3
Ermira Bajrami <ermirabajrami91@gmail.com>
2023-07-04 15:31:41 +0200

    bug fix

--- app/Services/NotificationSystemService.php
+++ app/Services/NotificationSystemService.php
@@ -8,24 +8,24 @@ class NotificationSystemService
 
     public function notificationSystem($userId)
     {
-        $notification = NotificationSystem::join('users', 'users.id', 'user_notification.user_id')
+        $notification = NotificationSystem::join('users', 'users.id', 'notification_system.user_id')
             ->where('users.id', $userId)
-            ->select('user_notification.system')
+            ->select('notification_system.system')
             ->first();
 
         $systemValue = (int) $notification->system;
 
         if ($systemValue == 1) {
-            NotificationSystem::join('users', 'users.id', 'user_notification.user_id')
+            NotificationSystem::join('users', 'users.id', 'notification_system.user_id')
                 ->where('users.id', $userId)
-                ->update(['user_notification.system' => 0]);
+                ->update(['notification_system.system' => 0]);
 
             return 'Notifications off';
         }
         if ($systemValue == 0) {
-            NotificationSystem::join('users', 'users.id', 'user_notification.user_id')
+            NotificationSystem::join('users', 'users.id', 'notification_system.user_id')
                 ->where('users.id', $userId)
-                ->update(['user_notification.system' => 1]);
+                ->update(['notification_system.system' => 1]);
 
             return 'Notifications on';
         }
