58fc1889af95fea2f42f5f80ad0a741f11e067cf
Ermira Bajrami <ermirabajrami91@gmail.com>
2023-07-04 08:33:17 +0200

    relationships

--- app/Models/product.php
+++ app/Models/product.php
@@ -27,4 +27,19 @@ class Product extends Model
         'company_id',
         'created_at',
     ];
+
+    public function file(): BelongsToMany
+    {
+        return $this->belongsToMany(FileUpload::class, 'file_has_product', 'file_id', 'product_id');
+    }
+
+    public function importProduct(): BelongsTo
+    {
+        return $this->belongsTo(ImportProduct::class, 'product_id');
+    }
+
+    public function exportProduct(): BelongsTo
+    {
+        return $this->belongsTo(ExportProduct::class, 'product_id');
+    }
 }
